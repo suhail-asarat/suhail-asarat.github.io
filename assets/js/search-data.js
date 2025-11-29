39a15ed9e106358c0391ed16ccb8e306aa612c31
5uhail <38782561+5uhail@users.noreply.github.com>
2025-11-29 03:32:39 +0000

    Deploying to gh-pages from @ 5uhail/5uhail.github.io@e899baa4a62bd6c2cb8e473a31622f8c83ff9d1b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
+        },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
